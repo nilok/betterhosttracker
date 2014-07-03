7b0e26db574904b3af4db3889e2dce638d459387
André Martins <aanm@ua.pt>
2014-07-03 03:04:50 +0100

    Tried to pull out addresses on datachangelistener

--- opendaylight/betterhosttracker/implementation/src/main/java/org/opendaylight/controller/betterhosttracker/plugin/internal/BetterHostTrackerImpl.java
+++ opendaylight/betterhosttracker/implementation/src/main/java/org/opendaylight/controller/betterhosttracker/plugin/internal/BetterHostTrackerImpl.java
@@ -1,12 +1,17 @@
 package org.opendaylight.controller.betterhosttracker.plugin.internal;
 
-import java.util.Map;
 import com.google.common.base.Preconditions;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 import org.opendaylight.controller.md.sal.common.api.data.DataChangeEvent;
 import org.opendaylight.controller.sal.binding.api.data.DataBrokerService;
 import org.opendaylight.controller.sal.binding.api.data.DataChangeListener;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.address.tracker.rev140617.AddressCapableNodeConnector;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.address.tracker.rev140617.address.node.connector.Addresses;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.betterhosttracker.rev140624.Host;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.node.NodeConnector;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
@@ -26,9 +31,12 @@ public class BetterHostTrackerImpl implements DataChangeListener {
 
     private DataBrokerService dataService;
 
+    private Set<Host> hosts;
+
     BetterHostTrackerImpl(DataBrokerService dataService) {
         Preconditions.checkNotNull(dataService, "dataBrokerService should not be null.");
         this.dataService = dataService;
+        this.hosts = Collections.synchronizedSet(new HashSet());
     }
 
     @Override
@@ -38,11 +46,25 @@ public class BetterHostTrackerImpl implements DataChangeListener {
         }
         Map<InstanceIdentifier<?>, DataObject> linkOriginalData = change.getOriginalOperationalData();
         Map<InstanceIdentifier<?>, DataObject> linkUpdatedData = change.getUpdatedOperationalData();
+        //aanm: we read from the updated operational data, right?
+        for (Map.Entry<InstanceIdentifier<?>, DataObject> entrySet : linkUpdatedData.entrySet()) {
+            InstanceIdentifier<?> key = entrySet.getKey();
+            DataObject dataObject = entrySet.getValue();
+            if (key.getClass().isInstance(InstanceIdentifier.class)
+                    && dataObject.getClass().isInstance(Addresses.class)) {
+                Addresses addrs = (Addresses) dataObject;
+                InstanceIdentifier<Addresses> iia = (InstanceIdentifier<Addresses>) key;
+                InstanceIdentifier<NodeConnector> iinc = iia.firstIdentifierOf(NodeConnector.class);
+                NodeConnector nodeConnector = (NodeConnector) dataService.readOperationalData(iinc);
+                //Host h = null;// = new Host();
+                //HostBuilder hb = new HostBuilder();
+                //hosts.add(hb);
+            }
+        }
         Iterator<DataObject> iterator = linkOriginalData.values().iterator();
         DataObject next = iterator.next();
         AddressCapableNodeConnector t = (AddressCapableNodeConnector) next;
         t.getAddresses().get(0).getIp();
-        //aanm: How to read an IP address from the AddressCapableNodeConnector, is the DataObject?
     }
 
     void close() {
@@ -50,10 +72,11 @@ public class BetterHostTrackerImpl implements DataChangeListener {
     }
 
     void registerAsDataChangeListener() {
-        InstanceIdentifier<AddressCapableNodeConnector> addrCapableNodeConnectors = //
+        InstanceIdentifier<Addresses> addrCapableNodeConnectors = //
                 InstanceIdentifier.builder(Nodes.class) //
                 .child(Node.class).child(NodeConnector.class) //
-                .augmentation(AddressCapableNodeConnector.class).build();
+                .augmentation(AddressCapableNodeConnector.class)//
+                .child(Addresses.class).build();
 
         dataService.registerDataChangeListener(addrCapableNodeConnectors, this);
     }
