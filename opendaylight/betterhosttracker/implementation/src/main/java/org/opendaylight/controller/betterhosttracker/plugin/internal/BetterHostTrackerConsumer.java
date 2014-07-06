80b216df756531b7eb804423e84519d72489ae94
Evan Zeller <evanrzeller@gmail.com>
2014-07-06 18:03:25 -0400

    Updated deprecated databroker interaction
    Signed-off-by: Evan Zeller <evanrzeller@gmail.com>

--- opendaylight/betterhosttracker/implementation/src/main/java/org/opendaylight/controller/betterhosttracker/plugin/internal/BetterHostTrackerConsumer.java
+++ opendaylight/betterhosttracker/implementation/src/main/java/org/opendaylight/controller/betterhosttracker/plugin/internal/BetterHostTrackerConsumer.java
@@ -2,11 +2,11 @@ package org.opendaylight.controller.betterhosttracker.plugin.internal;
 
 import org.opendaylight.controller.sal.binding.api.AbstractBindingAwareConsumer;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ConsumerContext;
-import org.opendaylight.controller.sal.binding.api.data.DataBrokerService;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.address.tracker.rev140617.address.node.connector.Addresses;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+/*import org.opendaylight.yang.gen.v1.urn.opendaylight.address.tracker.rev140617.address.node.connector.Addresses;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;*/
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,7 +27,7 @@ public class BetterHostTrackerConsumer extends AbstractBindingAwareConsumer
 
     @Override
     public void onSessionInitialized(ConsumerContext session) {
-        DataBrokerService dataService = session.<DataBrokerService>getSALService(DataBrokerService.class);
+        DataBroker dataService = session.<DataBroker>getSALService(DataBroker.class);
         betterHostTrackerImpl = new BetterHostTrackerImpl(dataService);
         betterHostTrackerImpl.registerAsDataChangeListener();
     }
